66f34c7064d21d2a44f24ad68d3204f43f571c5a
Mkeith495 <maxencekeithetem@gmail.com>
2026-04-16 14:52:24 +0200

    ci: run lint build and unit tests

--- src/module/order/orderTypeOrmRepository.ts
+++ src/module/order/orderTypeOrmRepository.ts
@@ -4,6 +4,7 @@ import { Order } from './Order';
 export class OrderTypeOrmRepository {
     async save(order: Order): Promise<Order> {
         const typeOrmRepository = AppDataSource.getRepository<Order>(Order);
-        await typeOrmRepository.save(order);
+        const savedOrder = await typeOrmRepository.save(order);
+        return savedOrder;
     }
 }
